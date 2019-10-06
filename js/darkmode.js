e668336617d91e15027ec4432548c22be2c0129d
LB <49623578+LegeBeker@users.noreply.github.com>
2019-10-06 20:35:04 +0200

    Fixed index pages of subpages

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.querySelector('.dark-mode-checkbox');
 
-  checkbox.checked = localStorage.getItem('darkMode') === 'true';
+  checkbox.checked = window.localStorage.getItem('darkMode') === 'true';
 
   checkbox.addEventListener('change', function (event) {
-    localStorage.setItem('darkMode', event.currentTarget.checked);
+    window.localStorage.setItem('darkMode', event.currentTarget.checked);
   });
 });
